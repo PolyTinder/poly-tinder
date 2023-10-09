ceb8bf3a97bce387825110716e946478b7b3f1b4
PolyTinder <poly.tinder.2023@gmail.com>
2023-10-09 15:49:55 +0200

    only fetch match if user is valid

--- packages/client/src/modules/matching/pages/matches-page/matches-page.component.ts
+++ packages/client/src/modules/matching/pages/matches-page/matches-page.component.ts
@@ -43,7 +43,11 @@ export class MatchesPageComponent {
             map((conversations) => conversations.length > 0),
         );
 
-        this.publicProfileService.fetchMatches().subscribe();
+        this.validationService.userValid.subscribe((userValid) => {
+            if (userValid) {
+                this.publicProfileService.fetchMatches().subscribe();
+            }
+        });
     }
 
     get userValid() {
