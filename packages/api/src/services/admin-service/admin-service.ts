a99c22c3ce9f32c17490c739492827a147e659b7
PolyTinder <poly.tinder.2023@gmail.com>
2023-10-11 16:45:14 +0200

    move reports method out of the main admin service

--- packages/api/src/services/admin-service/admin-service.ts
+++ packages/api/src/services/admin-service/admin-service.ts
@@ -2,7 +2,6 @@ import { singleton } from 'tsyringe';
 import { DatabaseService } from '../database-service/database-service';
 import { Knex } from 'knex';
 import { Admin } from 'common/models/admin';
-import { Report } from 'common/models/moderation';
 
 @singleton()
 export class AdminService {
@@ -12,33 +11,9 @@ export class AdminService {
         return this.databaseService.database('admin');
     }
 
-    private get reports(): Knex.QueryBuilder<Report> {
-        return this.databaseService.database('reports');
-    }
-
     async isAdmin(userId: number): Promise<boolean> {
         const admin = await this.admin.where({ userId }).first();
 
         return !!admin;
-    }
-
-    async getReports(): Promise<Report[]> {
-        return this.reports
-            .select([
-                'reports.*',
-                'reportingUser.userId as reportingUserId',
-                'reportedUser.userId as reportedUserId',
-            ])
-            .leftJoin(
-                'users as reportingUser',
-                'reports.reportingUserEmail',
-                'reportingUser.email',
-            )
-            .leftJoin(
-                'users as reportedUser',
-                'reports.reportedUserEmail',
-                'reportedUser.email',
-            )
-            .orderBy('created_at', 'desc');
     }
 }
