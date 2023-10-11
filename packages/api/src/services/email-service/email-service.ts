09e962276075b89deeb370cdc190a6650ad4f000
PolyTinder <146553756+PolyTinder@users.noreply.github.com>
2023-10-11 12:08:50 +0200

    output email to console in dev (#8)

--- packages/api/src/services/email-service/email-service.ts
+++ packages/api/src/services/email-service/email-service.ts
@@ -6,35 +6,98 @@ import { normaliseEmail } from '../../utils/email';
 
 @singleton()
 export class EmailService {
-    private mailjet: Client;
+    private mailjet: Client | undefined;
 
     constructor() {
         this.mailjet = this.init();
     }
 
     sendEmail(to: string, subject: string, content: string): Promise<unknown> {
-        return this.mailjet.post('send', { version: 'v3.1' }).request({
-            Messages: [
-                {
-                    From: {
-                        Email: env.EMAIL,
-                    },
-                    To: [
-                        {
-                            Email: normaliseEmail(to),
+        return this.haveAllRequiredEnvironmentVariables()
+            ? this.sendEmailWithMailJet(to, subject, content)
+            : this.sendEmailToConsole(to, subject, content);
+    }
+
+    private sendEmailWithMailJet(
+        to: string,
+        subject: string,
+        content: string,
+    ): Promise<unknown> {
+        return (
+            this.mailjet?.post('send', { version: 'v3.1' }).request({
+                Messages: [
+                    {
+                        From: {
+                            Email: env.EMAIL,
                         },
-                    ],
-                    Subject: subject,
-                    HTMLPart: content,
-                },
-            ],
-        });
+                        To: [
+                            {
+                                Email: normaliseEmail(to),
+                            },
+                        ],
+                        Subject: subject,
+                        HTMLPart: content,
+                    },
+                ],
+            }) ?? Promise.resolve()
+        );
+    }
+
+    private async sendEmailToConsole(
+        to: string,
+        subject: string,
+        content: string,
+    ): Promise<unknown> {
+        // eslint-disable-next-line no-console
+        console.log(
+            'Sending email:\n\tTo: ' +
+                to +
+                '\n\tSubject: ' +
+                subject +
+                '\n\tContent: ' +
+                content +
+                '\n',
+        );
+
+        return;
     }
 
     private init() {
-        return new MailJet({
-            apiKey: env.MJ_APIKEY_PUBLIC,
-            apiSecret: env.MJ_APIKEY_PRIVATE,
-        });
+        return this.checkEnvironment()
+            ? new MailJet({
+                  apiKey: env.MJ_APIKEY_PUBLIC,
+                  apiSecret: env.MJ_APIKEY_PRIVATE,
+              })
+            : undefined;
+    }
+
+    private checkEnvironment(): boolean {
+        if (!this.haveAllRequiredEnvironmentVariables()) {
+            if (env.NODE_ENV === 'production') {
+                throw new Error(
+                    'Missing required environment variables for email service',
+                );
+            } else {
+                // eslint-disable-next-line no-console
+                console.warn(
+                    'Missing required environment variables for email service. Defaulting to console logging for development.',
+                );
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private haveAllRequiredEnvironmentVariables(): boolean {
+        return (
+            env.EMAIL !== undefined &&
+            env.EMAIL.length > 0 &&
+            env.MJ_APIKEY_PRIVATE !== undefined &&
+            env.MJ_APIKEY_PRIVATE.length > 0 &&
+            env.MJ_APIKEY_PUBLIC !== undefined &&
+            env.MJ_APIKEY_PUBLIC.length > 0
+        );
     }
 }
